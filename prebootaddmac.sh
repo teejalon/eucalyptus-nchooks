7edf5396445ef9ca033ccb67b694ed61402d6bb3
Teemu Jalonen <teemu.jalonen@iki.fi>
2014-01-15 13:29:38 +0200

    Create prebootaddmac.sh
    extended from eucalyptus example.sh

---
+++ prebootaddmac.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Copyright 2009-2012 Eucalyptus Systems, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+# Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
+# CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
+# additional information or have any questions.
+#
+# This file may incorporate work covered under the following copyright
+# and permission notice:
+#
+#   Software License Agreement (BSD License)
+#
+#   Copyright (c) 2008, Regents of the University of California
+#   All rights reserved.
+#
+#   Redistribution and use of this software in source and binary forms,
+#   with or without modification, are permitted provided that the
+#   following conditions are met:
+#
+#     Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#     Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer
+#     in the documentation and/or other materials provided with the
+#     distribution.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#   POSSIBILITY OF SUCH DAMAGE. USERS OF THIS SOFTWARE ACKNOWLEDGE
+#   THE POSSIBLE PRESENCE OF OTHER OPEN SOURCE LICENSED MATERIAL,
+#   COPYRIGHTED MATERIAL OR PATENTED MATERIAL IN THIS SOFTWARE,
+#   AND IF ANY SUCH MATERIAL IS DISCOVERED THE PARTY DISCOVERING
+#   IT MAY INFORM DR. RICH WOLSKI AT THE UNIVERSITY OF CALIFORNIA,
+#   SANTA BARBARA WHO WILL THEN ASCERTAIN THE MOST APPROPRIATE REMEDY,
+#   WHICH IN THE REGENTS' DISCRETION MAY INCLUDE, WITHOUT LIMITATION,
+#   REPLACEMENT OF THE CODE SO IDENTIFIED, LICENSING OF THE CODE SO
+#   IDENTIFIED, OR WITHDRAWAL OF THE CODE CAPABILITY TO THE EXTENT
+#   NEEDED TO COMPLY WITH ANY SUCH LICENSES OR RIGHTS.
+
+
+# All executables placed into this directory will be invoked by NC
+# at certain points with certain arguments. Currently supported are:
+#
+#   euca-nc-pre-init      <eucalyptus-home>
+#   euca-nc-post-init     <eucalyptus-home>
+#   euca-nc-pre-hyp-check <eucalyptus-home>
+#   euca-nc-pre-boot      <eucalyptus-home> <instance-home>
+#   euca-nc-pre-adopt     <eucalyptus-home> <instance-home>
+#   euca-nc-pre-clean     <eucalyptus-home> <instance-home>
+#   euca-nc-pre-attach    <eucalyptus-home> <volume-Xml-Path>
+#   euca-nc-post-detach   <eucalyptus-home> <volume-Xml-Path>
+#
+# If the executables return 0, the execution proceeds normally.
+# If values [1-99] are returned, NC will abort some operation:
+# it will not initialize, or will not run or adopt the instance.
+# Any directories or files that cannot be executed are ignored.
+#
+# Make this bash script executable for it to work.
+
+event=$1
+euca_home=$2
+inst_home=$3
+
+if [ "$event" == "" ] ; then
+    echo "an NC hook is called without an event"
+    exit 1;
+fi
+
+if [ "$euca_home" == "" ] ; then
+    echo "an NC hook is called without EUCALYPTUS home"
+    exit 1;
+fi
+
+log_file=${euca_home}/var/log/eucalyptus/nc-hooks.log
+
+echo `date` "$event $euca_home $inst_home" >>$log_file
+
+rewrite_libvirt_xml() {
+
+        MAC=`/var/lib/eucalyptus/macgen.py`
+        echo `date` "$event $euca_home $inst_home $MAC" >> $log_file
+        sed  "s/REPLACEME/$MAC/" < $inst_home/instance-libvirt.xml > $inst_home/instance-libvirt.xml.new
+        mv -f $inst_home/instance-libvirt.xml.new $inst_home/instance-libvirt.xml
+}
+
+case "$event" in
+    euca-nc-pre-boot)
+        rewrite_libvirt_xml
+        exit 0
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
+exit 0;
